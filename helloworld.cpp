eb6b24e3c7f2c024c8eb25900948fa02a02bde01
Sumit Kumar Garsa <sumitkumargarsa@gmail.com>
2025-06-06 23:08:11 +0530

    comment statements
    important logics and concepts are included in the comment statements

--- helloworld.cpp
+++ helloworld.cpp
@@ -1,7 +1,16 @@
 #include<iostream>
+/*directive processed by pre processor here iostream  is a header file*/
 using namespace std;
-
+/* This line specifies that you have to include an standard file
+std.
+If we doesn't insert std then we have to right the statement like std::cout<<"Whatever I want to print";
+*/
 int main(){
-    cout<<"Hello World";
+    // Executable programme
+    /*Writing an programme in c++ return type function_name(parameters){
+    }*/
+    cout<<"Hello"
+    // Here the endl is used to print the  output in the next line of code 
+    <<endl<<"World";
     return 0;
 }
