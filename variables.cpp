8f11bb3a90caf7e7963c44c9708abb8ff2a66c3e
Sumit Kumar Garsa <sumitkumargarsa@gmail.com>
2025-06-07 00:59:07 +0530

    Create variables.cpp

---
+++ variables.cpp
@@ -0,0 +1,47 @@
+int apples = 4 /*here int is a variable type apples  is variable name*/
+
+/*C++ Naming Convention
+1. Lower Case
+2. Uppercase
+3. Camelcase
+Rule for naming variables in c++
+Rule 01 : Variable names should not begin with a number
+Rule 02 : Whitespace is not permitted in variable names
+Rule 03 : A C++ keyword(reversed word) cannot be used as a variable name
+Rule 04 : it is preferred to use variable names with more than one word with all lowercase letters for the first word and capitalization of the first letter of each subsequent word 
+Rule 05 : When creating variables, it is preferred to give them meaningful names{for example : int number Of Apples = 4 }
+Rule 06 : All lowercase letters should be used when creating one-word variable names.
+*/
+
+
+/*C++ Identifiers
+Names of any class variables methods
+Rule 01 : All identifiers should begin with a letter (A to Z or a to z), currency character($) or an underscore(_).
+Rule 02 : After the first character, identifiers can have any combination of characters.
+Rule 03 :  A keyword cannot be used as an identifier.
+Rule 04 : The Identifier are case sensitive.
+Rule 05 : Whitespace is not permitted.
+Examples of legal and illegal identifiers:
+1. 2_abc (Not Allowed)
+2. *Tea (Not Allowed)
+3. _2abc (Allowed)
+4. $Tea (Allowed)
+*/
+
+/*C++ Data types
+
+1. Primary(Built in) : Integer, Character , Boolean , Floating point , Double Floating Point , Void , Wide Character 
+
+2. Derived (Derived from Primary) : Function, Array, Pointer, Reference
+
+3. User Defined (Defined by user) : Class , Structure , Union , Enum , TypeDef
+
+*/
+
+/*
+Integer Data type
+
+1 byte = 8 bits (Bit : Smallest unit of storage)
+2,3,4
+int a  = 3;(4 bytes)
+*/
