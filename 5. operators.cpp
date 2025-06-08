b82ff3573e59d7581890773572db7f0d1de73a45
Sumit Kumar Garsa <sumitkumargarsa@gmail.com>
2025-06-09 01:35:21 +0530

    Assignment operator

--- 5. operators.cpp
+++ 5. operators.cpp
@@ -53,6 +53,11 @@ int main(){
     cout<<num1/num2<<endl;
     cout<<num1%num2<<endl;
 
+    num1+=3;
+    cout<<num1<<endl;
+    num2-=2;
+    cout<<num2<<endl;
+
     return 0;
 
 }
@@ -117,3 +122,33 @@ int main(){
     cout<<(!exp1)<<endl; //false
     return 0;
 }
+
+
+#include<iostream>
+using namespace std;
+int main(){
+
+    int num1 = 6;
+    int num2  = 3;
+
+    cout<<num1+num2<<endl;
+    cout<<num1-num2<<endl;
+    cout<<num1*num2<<endl;
+    cout<<num1/num2<<endl;
+    cout<<num1%num2<<endl;
+
+    num1+=3;
+    cout<<num1<<endl;
+    num2-=2;
+    cout<<num2<<endl;
+
+    return 0;
+
+}
+/* Assignment  Operators
+=   int p = 4; int q = p;
++=  a+= 1  a = a+1
+-=   a-= 1  a = a-1
+/= a\=1  a = a/1
+%= a%=1 a =  a%1
+*/
